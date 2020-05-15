50c0d5bda028427df8f1bd1e064088cb5e9c4265
LuigiRamires <54213955+LuigiRamires@users.noreply.github.com>
2020-05-15 20:56:56 -0300

    design

--- Calculadora/Calculadora/Form1.Designer.cs
+++ Calculadora/Calculadora/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calculadora
 {
-    partial class Form1
+    partial class frmCalculadora
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblCalculadora = new System.Windows.Forms.Label();
+            this.lblPrimeiroValor = new System.Windows.Forms.Label();
+            this.lblSegundoValor = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblCalculadora
+            // 
+            this.lblCalculadora.AutoSize = true;
+            this.lblCalculadora.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalculadora.Location = new System.Drawing.Point(74, 9);
+            this.lblCalculadora.Name = "lblCalculadora";
+            this.lblCalculadora.Size = new System.Drawing.Size(108, 21);
+            this.lblCalculadora.TabIndex = 0;
+            this.lblCalculadora.Text = "Calculadora";
+            // 
+            // lblPrimeiroValor
+            // 
+            this.lblPrimeiroValor.AutoSize = true;
+            this.lblPrimeiroValor.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrimeiroValor.Location = new System.Drawing.Point(12, 67);
+            this.lblPrimeiroValor.Name = "lblPrimeiroValor";
+            this.lblPrimeiroValor.Size = new System.Drawing.Size(70, 20);
+            this.lblPrimeiroValor.TabIndex = 1;
+            this.lblPrimeiroValor.Text = "1° Valor:";
+            // 
+            // lblSegundoValor
+            // 
+            this.lblSegundoValor.AutoSize = true;
+            this.lblSegundoValor.Font = new System.Drawing.Font("Century Gothic", 11.25F);
+            this.lblSegundoValor.Location = new System.Drawing.Point(12, 96);
+            this.lblSegundoValor.Name = "lblSegundoValor";
+            this.lblSegundoValor.Size = new System.Drawing.Size(70, 20);
+            this.lblSegundoValor.TabIndex = 2;
+            this.lblSegundoValor.Text = "2° Valor:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(105, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 13);
+            this.textBox2.TabIndex = 4;
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(275, 310);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblSegundoValor);
+            this.Controls.Add(this.lblPrimeiroValor);
+            this.Controls.Add(this.lblCalculadora);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCalculadora;
+        private System.Windows.Forms.Label lblPrimeiroValor;
+        private System.Windows.Forms.Label lblSegundoValor;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Calculadora/Calculadora/Form1.cs
+++ Calculadora/Calculadora/Form1.cs
@@ -10,11 +10,12 @@ using System.Windows.Forms;
 
 namespace Calculadora
 {
-    public partial class Form1 : Form
+    public partial class frmCalculadora : Form
     {
-        public Form1()
+        public frmCalculadora()
         {
             InitializeComponent();
         }
+
     }
 }
